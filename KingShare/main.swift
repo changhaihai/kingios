6c9d2165936663f3c484e313b97d26425ea17bf5
changhaihai <431249235@qq.com>
2026-09-06 18:26:00 +0800

    Fix SpringBoard global HUD hosting lifecycle

--- KingShare/main.swift
+++ KingShare/main.swift
@@ -9,12 +9,8 @@ if hudMode {
     hai_prepare_hud_plugin()
     let delegate = HUDHelperAppDelegate()
     UIApplication.shared.delegate = delegate
-    let index = CommandLine.arguments.firstIndex(of: "--state")
-    let statePath = index.flatMap { i in
-        let next = i + 1
-        return CommandLine.arguments.indices.contains(next) ? CommandLine.arguments[next] : nil
-    } ?? HUDTransport.stateURL().path
-    delegate.startHUD(statePath: statePath)
+    // __completeAndRunAsPlugin drives didFinishLaunchingWithOptions. Do not
+    // start the HUD manually here or invoke the plugin entry recursively.
     UIApplication.shared.perform(Selector(("__completeAndRunAsPlugin")))
     CFRunLoopRun()
     exit(0)
